501553715706e41e6c78355d62178322ea7ae487
Yash Raj Kohli <yashrajkohli5@gmail.com>
2025-12-18 10:40:46 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 import seaborn as sns
 import calendar
 import requests
